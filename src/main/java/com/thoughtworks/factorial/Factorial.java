18299dbc052f5fa865700ebbeb3c45c22f33b62c
Jess Mann <jmann@smith.edu>
2015-06-08 11:42:08 -0400

    refactored compute()

--- src/main/java/com/thoughtworks/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/factorial/Factorial.java
@@ -2,14 +2,14 @@ package com.thoughtworks.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-        if(i>2){
-            return i*2;
-        } else if (i>0){
-            return i;
-        } else if (i==0){
-            return 1;
-        } else {
+        if (i < 0) {
             throw new IllegalArgumentException();
+        } else if (i < 2) {
+            return 1;
+        } else if(i>2){
+            return i*2;
+        } else {
+            return compute(i-2)+compute(i-1);
         }
     }
 }
