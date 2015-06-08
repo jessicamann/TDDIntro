a0219a4ce7a121e28fe363f1c72141b62276a503
Jess Mann <jmann@smith.edu>
2015-06-08 12:58:02 -0400

    finished library tests. TODO: refactor & extract duplication

--- src/test/java/com/thoughtworks/library/LibraryTest.java
+++ src/test/java/com/thoughtworks/library/LibraryTest.java
@@ -2,6 +2,7 @@ package com.thoughtworks.library;
 
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormatter;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.PrintStream;
@@ -9,9 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.Matchers.contains;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class LibraryTest {
 
@@ -21,32 +20,43 @@ public class LibraryTest {
         List books tests. Implement the first three tests for the Verify exercise
 
      */
+    private List<String> books;
+    private PrintStream printStream;
+    private Library library;
 
+    @Before
+    public void setup(){
+        books = new ArrayList<>();
+        printStream = mock(PrintStream.class);
+        books.add("Book Title");
+        library = new Library(books, printStream, null);
+    }
 
     @Test
     public void shouldPrintBookTitleWhenThereIsOneBook() {
 
-        List<String> books = new ArrayList<>();
-        String title = "Book Title";
-        books.add(title);
-        PrintStream printStream = mock(PrintStream.class);
-        Library library = new Library(books, printStream, null);
-
         library.listBooks();
 
-        // add a verify statement here that shows that the book title was printed by to the printStream
+        verify(printStream).println("Book Title");
     }
 
     @Test
     public void shouldPrintNothingWhenThereAreNoBooks() {
 
-        // implement me
+        library = new Library(new ArrayList<String>(), printStream, null);
+        library.listBooks();
+
+        verifyZeroInteractions(printStream);
     }
 
     @Test
     public void shouldPrintBothBookTitlesWhenThereAreTwoBooks() {
 
-        // implement me
+        books.add("Book Title");
+
+        library.listBooks();
+
+        verify(printStream, times(2)).println("Book Title");
     }
 
     /*
@@ -59,15 +69,13 @@ public class LibraryTest {
     // This one is done for you
     @Test
     public void shouldWelcomeUser() {
-        List<String> books = new ArrayList<>();
-        PrintStream printStream = mock(PrintStream.class);
         DateTimeFormatter dateTimeFormatter = mock(DateTimeFormatter.class);
         Library library = new Library(books, printStream, dateTimeFormatter);
 
         // We don't need to mock DateTime because it is a value object
         // We can't mock it because it is a final class
         DateTime time = new DateTime();
-        
+
         library.welcome(time);
         
         verify(printStream).println(contains("Welcome"));
@@ -75,23 +83,25 @@ public class LibraryTest {
     
     @Test
     public void shouldDisplayFormattedTime() {
-        List<String> books = new ArrayList<>();
-        PrintStream printStream = mock(PrintStream.class);
         DateTime time = new DateTime();
         DateTimeFormatter dateTimeFormatter = mock(DateTimeFormatter.class);
 
-        when(dateTimeFormatter.print(time)).thenReturn("FormattedTimeString");
+        when(dateTimeFormatter.print(time)).thenReturn("2013-04-08 16:33:17");
 
         Library library = new Library(books, printStream, dateTimeFormatter);
 
         library.welcome(time);
 
-        // add a verify here
+        verify(dateTimeFormatter).print(time);
     }
 
     @Test
     public void shouldDisplayFormattedTimeWhenItIsAnEmptyString() {
-
-        // implement me
+        DateTime time = null;
+        DateTimeFormatter dateTimeFormatter = mock(DateTimeFormatter.class);
+        when(dateTimeFormatter.print(time)).thenReturn("2013-04-08 16:33:17");
+        library = new Library(books, printStream, dateTimeFormatter);
+        library.welcome(time);
+        verify(printStream).println(contains("2013-04-08 16:33:17"));
     }
 }
