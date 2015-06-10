85361a38e1ea09ac477547265144cd0367ff6916
Jess Mann <jmann@smith.edu>
2015-06-09 23:39:00 -0400

    redid factorial test

--- src/main/java/com/thoughtworks/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/factorial/Factorial.java
@@ -2,14 +2,12 @@ package com.thoughtworks.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-        if (i < 0) {
-            throw new IllegalArgumentException();
-        } else if (i < 2) {
+        if(i==0){
             return 1;
-        } else if(i>2){
-            return i*2;
-        } else {
-            return compute(i-2)+compute(i-1);
+        } else if (i>0) {
+            return i * compute(i - 1);
+        }  else {
+            throw new IllegalArgumentException();
         }
     }
 }
